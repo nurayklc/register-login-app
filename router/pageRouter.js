f6a47e5162450f04557addeb5952a20f26678859
nurayklc <nuraykilic0101@gmail.com>
2022-04-07 23:42:11 +0300

    Fix page router and controller

--- router/pageRouter.js
+++ router/pageRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const pageController = require('../controller/pageController');
-const redirectMiddleware = require('../middlewares/redirectMiddleware')
 
 router.route('/register').get(pageController.getRegisterPage);
 router.route('/login').get(pageController.getLoginPage);
